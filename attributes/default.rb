3708ca6f775752ddfb11eae3ad54d21cfefb5cdf
Alexander De Leon <me@alexdeleon.name>
2015-04-21 15:43:32 +0200

    Added configuration support

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,7 @@ default[:neo4j][:docker_image]="tpires/neo4j"
 default[:neo4j][:docker_image_tag]="latest"
 default[:neo4j][:docker_container]="neo4j"
 default[:neo4j][:data_path]="/var/data/neo4j"
+default[:neo4j][:config_path]="/etc/neo4j"
 default[:neo4j][:container_data_path]="/var/lib/neo4j/data"
+default[:neo4j][:container_config_path]="/var/lib/neo4j/config"
+default[:neo4j][:config]={}
--- recipes/default.rb
+++ recipes/default.rb
@@ -11,6 +11,18 @@ directory node[:neo4j][:data_path] do
 	recursive true
 	action :create
 end
+directory node[:neo4j][:config_path] do
+	recursive true
+	action :create
+end
+
+# Build the configuration
+template "#{node[:neo4j][:config_path]}/neo4j.properties" do
+	source "neo4j.properties.erb"
+	variables :config => node[:neo4j][:config]
+	action :create
+	notifies :restart, "service[neo4j]", :delayed
+end
 
 # Run the docker container
 docker_container "neo4j" do
@@ -20,5 +32,10 @@ docker_container "neo4j" do
 	detach true
 	port ['7474:7474', '1337:1337']
 	volume [
+		"#{node[:neo4j][:config_path]}:#{node[:neo4j][:container_config_path]}",
 		"#{node[:neo4j][:data_path]}:#{node[:neo4j][:container_data_path]}"]
+end
+
+service "neo4j" do
+	action :nothing
 end
