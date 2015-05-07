4ea3dfe7de2220a9e59d95ea0a5f65e19a17071a
Alexander De Leon <me@alexdeleon.name>
2015-05-07 11:47:07 +0200

    Correct version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'me@alexdeleon.name'
 license          'MIT'
 description      'Installs/Configures Neo4J docker container'
 long_description 'Installs/Configures Neo4J docker container'
-version          '0.1.1'
+version          '0.1.3'
 
 depends "docker"
